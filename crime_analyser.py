42b0b3a1e4e2e3ff46d6f47b7592b8bc0e56a055
Ahyan <307210285+NightEyes0@users.noreply.github.com>
2026-09-16 17:34:52 +0100

    Add sample dataset and load it using Pandas

--- crime_analyser.py
+++ crime_analyser.py
@@ -1,1 +1,9 @@
-print("Crime Data Analyser initialized!")
+import pandas as pd
+
+print("Loading crime data...")
+
+# Read the CSV file into a Pandas DataFrame
+data = pd.read_csv("crime_data.csv")
+
+# Print the table /output
+print(data)
